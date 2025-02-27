fddf1220b937b88b0514e7487560b1098a812768
홍종훈 <whdgnszz1@snu.ac.kr>
2025-02-27 19:49:01 +0900

    feat: 편집거리를 이용하여 평가하도록 평가방법 수정

--- code/evaluation.py
+++ code/evaluation.py
@@ -2,104 +2,73 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 import datasets
 import pandas as pd
-
 import os
 import sys
 import json
 from tqdm import tqdm
 from datetime import datetime
 import argparse
+import Levenshtein
 
 
 def load_datasets(test_file):
-    # load data and make datasets
     with open(test_file, 'r') as f:
         json_dataset = json.load(f)
-    
+
     list_dataset = {
-        'id': list(map(lambda x: x['metadata_info']['id'], json_dataset['data'])),
-        'err_sentence': list(map(lambda x: x['annotation']['err_sentence'], json_dataset['data'])),
-        'cor_sentence': list(map(lambda x: x['annotation']['cor_sentence'], json_dataset['data']))
+        'id': [data['metadata_info']['id'] for data in json_dataset['data']],
+        'err_sentence': [data['annotation']['err_sentence'] for data in json_dataset['data']],
+        'cor_sentence': [data['annotation']['cor_sentence'] for data in json_dataset['data']]
     }
-    
+
     dataset_dict = {
         'test': datasets.Dataset.from_dict(list_dataset, split='test')
     }
-    dataset = datasets.DatasetDict(dataset_dict)
-    return dataset
+    return datasets.DatasetDict(dataset_dict)
 
-def get_ngram(text, n_gram):
-    ngram_list = []
-    text_length = len(text)
-    for i in range(text_length - n_gram + 1):
-        ngram_list.append(text[i:i+n_gram])
-    return ngram_list
 
-def calc_f_05(cor_sentence, prd_sentence, n_gram):
-    prd_word_list = get_ngram(prd_sentence, n_gram)
-    cor_word_list = get_ngram(cor_sentence, n_gram)
+def calc_accuracy(cor_sentence, prd_sentence):
+    """예측 문장과 정답 문장이 완전히 일치하면 1.0, 아니면 0.0"""
+    return 1.0 if prd_sentence == cor_sentence else 0.0
 
-    if not prd_word_list or not cor_word_list:
-        return 0, 0, 0
 
-    cnt = 0
-    for idx in range(len(prd_word_list)):
-        start_idx = 0
-        end_idx = idx + 2
-        if idx > 2:
-            start_idx = idx - 2
-        if prd_word_list[idx] in cor_word_list[start_idx:end_idx]:
-            cnt += 1
+def calc_edit_distance(cor_sentence, prd_sentence):
+    """정답 문장과 예측 문장 간의 편집 거리"""
+    return Levenshtein.distance(cor_sentence, prd_sentence)
 
-    precision = cnt / len(prd_word_list)
-    recall = cnt / len(cor_word_list)
 
-    if precision + recall == 0:
-        return 0, 0, 0
+def calc_char_accuracy(cor_sentence, prd_sentence):
+    """문자 단위 정확도: 일치하는 문자 수 / 정답 문장 길이"""
+    if not cor_sentence:
+        return 0.0
+    matches = sum(1 for c1, c2 in zip(cor_sentence, prd_sentence) if c1 == c2)
+    return matches / len(cor_sentence)
 
-    f_05 = 1.25 * (precision * recall) / (0.25 * precision + recall)
-
-    return precision, recall, f_05
 
 def my_train(gpus='cpu', model_path=None, test_file=None, eval_length=None, save_path=None, pb=False):
-    # Load model and tokenizer
     model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
     tokenizer = AutoTokenizer.from_pretrained(model_path)
-    
-    # load dataset
+
     dataset = load_datasets(test_file)
-    
-    # model to device
+
     device = torch.device(gpus)
     model.to(device)
-    
-    # inference data and calculate scores(precision, recall, f0.5)
-    id_list = []
-    err_sentence_list = []
-    cor_sentence_list = []
-    prd_sentence_list = []
-    precision_list = []
-    recall_list = []
-    f_05_list = []
-    
-    ngram = 5
+
+    id_list, err_sentence_list, cor_sentence_list, prd_sentence_list = [], [], [], []
+    accuracy_list, edit_distance_list, char_accuracy_list = [], [], []
+
     data_len = len(dataset['test'])
-    bar_length = 100
     if eval_length:
-        data_len = min(eval_length, len(dataset['test']))
-    _per_calc = 0.0
-    
-    print('=' * bar_length)
+        data_len = min(eval_length, data_len)
+
+    print('=' * 100)
     for n in tqdm(range(data_len), disable=pb):
         data_id = dataset['test'][n]['id']
-        id_list.append(data_id)
         err_sentence = dataset['test'][n]['err_sentence']
-        err_sentence_list.append(err_sentence)
         cor_sentence = dataset['test'][n]['cor_sentence']
-        cor_sentence_list.append(cor_sentence)
+
         tokenized = tokenizer(err_sentence, return_tensors='pt')
-        input_ids = tokenized['input_ids']
-        input_ids = input_ids.to(device)
+        input_ids = tokenized['input_ids'].to(device)
         res = model.generate(
             inputs=input_ids,
             num_beams=20,
@@ -108,59 +77,62 @@ def my_train(gpus='cpu', model_path=None, test_file=None, eval_length=None, save
             repetition_penalty=0.2,
             length_penalty=0.2,
             no_repeat_ngram_size=2,
-            max_length=input_ids.size()[1] + 5).cpu().tolist()[0]
+            max_length=input_ids.size()[1] + 5
+        ).cpu().tolist()[0]
         prd_sentence = tokenizer.decode(res).replace('<pad>', '').replace('<s>', '').replace('</s>', '').strip()
+
+        id_list.append(data_id)
+        err_sentence_list.append(err_sentence)
+        cor_sentence_list.append(cor_sentence)
+        prd_sentence_list.append(prd_sentence)
+
+        # 정확도, 편집 거리, 문자 단위 정확도 계산
+        accuracy = calc_accuracy(cor_sentence, prd_sentence)
+        edit_distance = calc_edit_distance(cor_sentence, prd_sentence)
+        char_accuracy = calc_char_accuracy(cor_sentence, prd_sentence)
+        accuracy_list.append(accuracy)
+        edit_distance_list.append(edit_distance)
+        char_accuracy_list.append(char_accuracy)
+
         _cnt = n + 1
         _per_calc = round(_cnt / data_len, 4)
         _now_time = datetime.now().__str__()
-        _blank = ' ' * 30
         print(f'[{_now_time}] - [{_per_calc:6.1%} {_cnt:06,}/{data_len:06,}] - Evaluation Result (Data id : {data_id})')
-        print(f'{_blank} >       TEST : {err_sentence}')
-        print(f'{_blank} >    PREDICT : {prd_sentence}')
-        print(f'{_blank} >      LABEL : {cor_sentence}')
-        
-        # calculate scores
-        precision, recall, f_05 = calc_f_05(cor_sentence, prd_sentence, ngram)
-        precision_list.append(precision)
-        recall_list.append(recall)
-        f_05_list.append(f_05)
-        print(f'{_blank} >  PRECISION : {precision:6.3f}')
-        print(f'{_blank} >     RECALL : {recall:6.3f}')
-        print(f'{_blank} > F0.5 SCORE : {f_05:6.3f}')
-        print('=' * bar_length)
-        prd_sentence_list.append(prd_sentence)
+        print(f'{" " * 30} >       TEST : {err_sentence}')
+        print(f'{" " * 30} >    PREDICT : {prd_sentence}')
+        print(f'{" " * 30} >      LABEL : {cor_sentence}')
+        print(f'{" " * 30} > ACCURACY : {accuracy:6.3f}')
+        print(f'{" " * 30} > EDIT DISTANCE : {edit_distance}')
+        print(f'{" " * 30} > CHAR ACCURACY : {char_accuracy:6.3f}')
+        print('=' * 100)
+
         torch.cuda.empty_cache()
-    
-    # results save
-    _now_time = datetime.now().__str__()
+
     save_file_name = os.path.split(test_file)[-1].replace('.json', '') + '.csv'
     save_file_path = os.path.join(save_path, save_file_name)
-    _df = pd.DataFrame({
-        'id': data_id,
+    df = pd.DataFrame({
+        'id': id_list,
         'err_sentence': err_sentence_list,
         'prd_sentence': prd_sentence_list,
         'cor_sentence': cor_sentence_list,
-        'precision': precision_list,
-        'recall': recall_list,
-        'f_05': f_05_list
+        'accuracy': accuracy_list,
+        'edit_distance': edit_distance_list,
+        'char_accuracy': char_accuracy_list
     })
-    _df.to_csv(save_file_path, index=True)
-    print(f'[{_now_time}] - Save Result File(.csv) - {save_file_path}')
-    
-    print('=' * bar_length)
-    # calculate presision, recall, F0.5 score by ngram-6
-    mean_precision = sum(precision_list) / len(precision_list)
-    mean_recall = sum(recall_list) / len(recall_list)
-    mean_f_05 = sum(f_05_list) / len(f_05_list)
-    
-    print(f'       Evaluation Ngram : {ngram}')
-    print(f'      Average Precision : {mean_precision:6.3f}')
-    print(f'         Average Recall : {mean_recall:6.3f}')
-    print(f'     Average F0.5 score : {mean_f_05:6.3f}')
-    print('=' * bar_length)
+    df.to_csv(save_file_path, index=True)
+    print(f'[{datetime.now()}] - Save Result File(.csv) - {save_file_path}')
+
+    print('=' * 100)
+    mean_accuracy = sum(accuracy_list) / len(accuracy_list)
+    mean_edit_distance = sum(edit_distance_list) / len(edit_distance_list)
+    mean_char_accuracy = sum(char_accuracy_list) / len(char_accuracy_list)
+    print(f'       Average Accuracy : {mean_accuracy:6.3f}')
+    print(f'       Average Edit Distance : {mean_edit_distance:6.3f}')
+    print(f'       Average Char Accuracy : {mean_char_accuracy:6.3f}')
+    print('=' * 100)
+
 
 if __name__ == '__main__':
-    # parse inputs args
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpu_no", dest="gpu_no", type=int, action="store")
     parser.add_argument("--model_path", dest="model_path", type=str, action="store")
@@ -168,29 +140,19 @@ if __name__ == '__main__':
     parser.add_argument("--eval_length", dest="eval_length", type=int, action="store")
     parser.add_argument("-pb", dest="pb", action="store_true")
     args = parser.parse_args(sys.argv[1:])
-    
-    # make save path
+
     save_path = './data/results'
     os.makedirs(save_path, exist_ok=True)
-    
-    # get device
+
     gpu_no = 'cpu'
-    if args.gpu_no or args.gpu_no == 0:
+    if args.gpu_no is not None:
         gpu_no = f'cuda:{args.gpu_no}'
-    
-    # get pb value
-    if args.pb:
-        args.pb = False
-    else:
-        args.pb = True
-    
-    _now_time = datetime.now().__str__()
-    print(f'[{_now_time}] ========== Evaluation Start ==========')
-    
-    # call main method
-    print(f'DEVICE : {gpu_no}, MODEL PATH : {args.model_path}, FILE PATH : {args.test_file}, DATA LENGTH : {args.eval_length}, SAVE PATH : {save_path}')
-    my_train(gpu_no, model_path=args.model_path, test_file=args.test_file, eval_length=args.eval_length, save_path=save_path, pb=args.pb)
-    
-    _now_time = datetime.now().__str__()
-    print(f'[{_now_time}] ========== Evaluation Finished ==========')
-    
+
+    pb = not args.pb
+
+    print(f'[{datetime.now()}] ========== Evaluation Start ==========')
+    print(
+        f'DEVICE : {gpu_no}, MODEL PATH : {args.model_path}, FILE PATH : {args.test_file}, DATA LENGTH : {args.eval_length}, SAVE PATH : {save_path}')
+    my_train(gpu_no, model_path=args.model_path, test_file=args.test_file, eval_length=args.eval_length,
+             save_path=save_path, pb=pb)
+    print(f'[{datetime.now()}] ========== Evaluation Finished ==========')
